8b6306abd193059766f2970793d0a92596968dfd
KetanLogInt <ketan.logint@gmail.com>
2023-01-09 11:26:32 +0530

    New Template

---
+++ QMSWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace QMSWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
